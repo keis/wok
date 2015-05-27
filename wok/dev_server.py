9f85f310981747d52690bf8b61c1485589a2ad53
David Keijser <david.keijser@klarna.com>
2015-05-27 13:03:58 +0200

    python3 friendly print statements

--- wok/dev_server.py
+++ wok/dev_server.py
@@ -55,19 +55,19 @@ class dev_server:
 
         print("Starting dev server on http://%s:%s... (Ctrl-C to stop)"
                 %(socket_info[0], socket_info[1]))
-        print "Serving files from", self.serv_dir
+        print("Serving files from %s" % self.serv_dir)
 
         if self.dir_mon:
-            print "Monitoring the following directories for changes: "
+            print("Monitoring the following directories for changes: ")
             for d in self.watch_dirs:
-                print "\t", d
+                print("\t%s" % (d,))
         else:
-            print "Directory monitoring is OFF"
+            print("Directory monitoring is OFF")
 
         try:
             httpd.serve_forever()
         except KeyboardInterrupt:
-            print "\nStopping development server..."
+            print("\nStopping development server...")
 
 
 class RebuildHandlerWrapper(object):
